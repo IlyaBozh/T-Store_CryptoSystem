32c33f6630ef72a63dcd913a16f626b80bf821f5
Ilya <ilia.il.27@mail.ru>
2023-05-22 16:41:04 +0300

    Add Stored Procedure Transaction_SelectById

---
+++ T-Store_CryptoSystem.DB/dbo/Stored Procedures/Transaction_SelectById.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[Transaction_SelectById]
+		@Id bigint
+
+AS
+BEGIN
+
+	SELECT [Id],
+		   [AccountId],
+		   [Date],
+		   [TransactionType],
+		   [Amount],
+		   [Currency]
+	FROM [dbo].[Transaction]
+	WHERE [Id] = @Id
+END
