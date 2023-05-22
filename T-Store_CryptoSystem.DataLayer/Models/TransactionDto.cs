fee182584f2b30f287519b9d8284b3f244705da2
Ilya <ilia.il.27@mail.ru>
2023-05-22 17:39:05 +0300

    Add TransactionDto

---
+++ T-Store_CryptoSystem.DataLayer/Models/TransactionDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace T_Store_CryptoSystem.DataLayer.Models;
+
+public class TransactionDto
+{
+    public long Id { get; set; }
+    public long AccountId { get; set; }
+    public DateTime Date { get; set; }
+    public TransactionType TransactionType { get; set; }
+    public decimal Amount { get; set; }
+    public Currency Currency { get; set; }
+}
