5d830ed11ab27484d3a54556e5c110106e5fc405
Ilya <ilia.il.27@mail.ru>
2023-05-22 18:03:34 +0300

    Create & implement interface ITransactionRepository

---
+++ T-Store_CryptoSystem.DataLayer/Repository/interfaces/ITransactionRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using T_Store_CryptoSystem.DataLayer.Models;
+
+namespace T_Store_CryptoSystem.DataLayer.Repository.interfaces;
+
+public interface ITransactionRepository
+{
+    public Task<long> AddTransaction(TransactionDto transaction);
+    public Task<decimal> GetBalanceByAccountId(long accountId);
+    public Task<TransactionDto?> GetTransactionById(long id);
+    public Task<List<TransactionDto>> GetAllTransactionsByAccountId(long accountId);
+}
