b16eaee27c80b50cd42b9d7eaeaac2875ba79c7f
Ilya <ilia.il.27@mail.ru>
2023-05-22 17:53:11 +0300

    Add TransactionStoredProcedure

---
+++ T-Store_CryptoSystem.DataLayer/TransactionStoredProcedure.cs
@@ -0,0 +1,11 @@
+﻿
+namespace T_Store_CryptoSystem.DataLayer;
+
+public static class TransactionStoredProcedure
+{
+    public const string Transaction_Add = "Transaction_Add";
+    public const string Transaction_GetAllTransactionsByAccountId = "Transaction_GetAllTransactionsByAccountId";
+    public const string Transaction_GetBalanceByAccountId = "Transaction_GetBalanceByAccountId";
+    public const string Transaction_GetById = "Transaction_GetById";
+    public const string Transaction_GetLastTransactionByAccountId = "Transaction_GetLastTransactionByAccountId";
+}
