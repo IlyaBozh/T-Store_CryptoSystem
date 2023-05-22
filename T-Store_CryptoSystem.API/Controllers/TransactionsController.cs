67c7adeec42a18d5f4ab7144085d06726b30f505
Ilya <ilia.il.27@mail.ru>
2023-05-22 23:46:40 +0300

    Add TransactionsController

---
+++ T-Store_CryptoSystem.API/Controllers/TransactionsController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using T_Store_CryptoSystem.BusinessLayer.Services.Interfaces;
+
+namespace T_Store_CryptoSystem.API.Controllers;
+
+[ApiController]
+[Produces("application/json")]
+[Route("transactions")]
+public class TransactionsController : Controller
+{
+    private readonly ITransactionService _transactionServices;
+    private readonly IMapper _mapper;
+    private readonly ILogger<TransactionsController> _logger;
+
+    public TransactionsController(ITransactionService transactionServices, IMapper mapper, ILogger<TransactionsController> logger)
+    {
+        _transactionServices = transactionServices;
+        _mapper = mapper;
+        _logger = logger;
+    }
+}
