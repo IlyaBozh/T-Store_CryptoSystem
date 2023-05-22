19dfbb323b250b4deb514b2a1af95eac56200c4a
Ilya <ilia.il.27@mail.ru>
2023-05-22 19:29:28 +0300

    Create & implement interface ICalculationService

---
+++ T-Store_CryptoSystem.BusinessLayer/Services/CalculationService.cs
@@ -0,0 +1,13 @@
+﻿
+using T_Store_CryptoSystem.BusinessLayer.Models;
+using T_Store_CryptoSystem.BusinessLayer.Services.Interfaces;
+
+namespace T_Store_CryptoSystem.BusinessLayer.Services;
+
+public class CalculationService : ICalculationService
+{
+    public Task<List<TransactionModel>> ConvertCurrency(List<TransactionModel> transferModels)
+    {
+        throw new NotImplementedException();
+    }
+}
