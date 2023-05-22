f341e84f97f91ac65920a3b28316004ea5bd9333
Ilya <ilia.il.27@mail.ru>
2023-05-22 18:12:12 +0300

    Add method AddTransaction

--- T-Store_CryptoSystem.DataLayer/Repository/TransactionRepository.cs
+++ T-Store_CryptoSystem.DataLayer/Repository/TransactionRepository.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Dapper;
+using Microsoft.Extensions.Logging;
+using System.Data;
+using T_Store_CryptoSystem.DataLayer.Models;
+using T_Store_CryptoSystem.DataLayer.Repository.interfaces;
 
-namespace T_Store_CryptoSystem.DataLayer.Repository
+namespace T_Store_CryptoSystem.DataLayer.Repository;
+
+public class TransactionRepository : BaseRepository, ITransactionRepository
 {
-    internal class TransactionRepository
+    private readonly ILogger<TransactionRepository> _logger;
+
+    public TransactionRepository(IDbConnection dbConnection, ILogger<TransactionRepository> logger)
+        : base(dbConnection)
     {
+        _logger = logger;
+    }
+
+    public async Task<long> AddTransaction(TransactionDto transaction)
+    {
+        _logger.LogInformation("Data layer: Connection to data base");
+        var parameters = new DynamicParameters(new
+        {
+            transaction.AccountId,
+            transaction.TransactionType,
+            transaction.Amount,
+            transaction.Currency
+        });
+        parameters.Add("@Date", transaction.Date, DbType.DateTime2);
+
+        var id = await _dbConnection.QueryFirstOrDefaultAsync<long>(
+                  TransactionStoredProcedure.Transaction_Add,
+                  param: parameters,
+                  commandType: CommandType.StoredProcedure);
+
+        _logger.LogInformation($"Data layer: Transaction {transaction.TransactionType} id {id} created");
+        return id;
+    }
+
+    public Task<List<TransactionDto>> GetAllTransactionsByAccountId(long accountId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<decimal> GetBalanceByAccountId(long accountId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TransactionDto?> GetTransactionById(long id)
+    {
+        throw new NotImplementedException();
     }
 }
