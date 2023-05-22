21038e525adc5aa06e8ec545359981b08aeefce9
Ilya <ilia.il.27@mail.ru>
2023-05-22 17:47:10 +0300

    Add BaseRepository

---
+++ T-Store_CryptoSystem.DataLayer/BaseRepository.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Data;
+using System.Data.SqlClient;
+
+namespace T_Store_CryptoSystem.DataLayer;
+
+public class BaseRepository
+{
+    private readonly IDbConnection _connection;
+
+    public BaseRepository(IDbConnection dbConnection)
+    {
+        _connection = dbConnection;
+    }
+
+    protected IDbConnection _dbConnection => new SqlConnection(_connection.ConnectionString);
+}
