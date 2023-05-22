1ee9e5f5ce404c478aba2d9e2ef77cbccd3ecc87
Ilya <ilia.il.27@mail.ru>
2023-05-22 19:32:21 +0300

    Create & implement interface IRateService

---
+++ T-Store_CryptoSystem.BusinessLayer/Services/RateService.cs
@@ -0,0 +1,22 @@
+﻿
+using T_Store_CryptoSystem.BusinessLayer.Services.Interfaces;
+
+namespace T_Store_CryptoSystem.BusinessLayer.Services;
+
+public class RateService : IRateService
+{
+    public decimal GetCurrencyRate(string currencyFirst, string currencySecond)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Dictionary<string, decimal> GetRate()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SaveCurrencyRate(Dictionary<string, decimal> rates)
+    {
+        throw new NotImplementedException();
+    }
+}
