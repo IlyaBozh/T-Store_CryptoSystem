85c25fa8463732d590a852105fb5d7a285f8e050
Ilya <ilia.il.27@mail.ru>
2023-05-22 20:01:38 +0300

    Add MapperConfigBusiness

---
+++ T-Store_CryptoSystem.BusinessLayer/MapperConfig/MapperConfigBusiness.cs
@@ -0,0 +1,15 @@
+﻿
+using AutoMapper;
+using T_Store_CryptoSystem.BusinessLayer.Models;
+using T_Store_CryptoSystem.DataLayer.Models;
+
+namespace T_Store_CryptoSystem.BusinessLayer.MapperConfig;
+
+public class MapperConfigBusiness : Profile
+{
+    public MapperConfigBusiness()
+    {
+        CreateMap<TransactionModel, TransactionDto>().ReverseMap();
+        /*CreateMap<TransactionModel, TransactionCreatedEvent>();*/
+    }
+}
