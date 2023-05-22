a6689ca836551a4a50f8c4f9a8e90bba0b556a2b
Ilya <ilia.il.27@mail.ru>
2023-05-22 23:36:52 +0300

    Add ControllerExtensions

---
+++ T-Store_CryptoSystem.API/Extensions/ControllerExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace T_Store_CryptoSystem.API.Extensions;
+
+public static class ControllerExtensions
+{
+    public static string GetRequestPath(this ControllerBase controller) =>
+            $"{controller.Request?.Scheme}://{controller.Request?.Host.Value}{controller.Request?.Path.Value}";
+}
