ce8bed37373f0bcf9a2841eef9d3310f7287d4f6
Ilya <ilia.il.27@mail.ru>
2023-05-22 16:42:59 +0300

    Add Stored Procedure Transaction_SelectBalanceByAccountId

---
+++ T-Store_CryptoSystem.DB/dbo/Stored Procedures/Transaction_SelectBalanceByAccountId.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[Transaction_SelectBalanceByAccountId]
+	@AccountId bigint
+
+AS
+BEGIN
+
+	SELECT coalesce(sum([Amount]),0)
+	FROM [dbo].[Transaction]
+	WHERE [AccountId] = @AccountId
+END
