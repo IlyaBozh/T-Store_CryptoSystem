372c7d999835701c68389278ddd355e26eaa558e
Ilya <ilia.il.27@mail.ru>
2023-05-22 19:47:13 +0300

    Creat & implement interface ITransactionService

---
+++ T-Store_CryptoSystem.BusinessLayer/Services/TransactionService.cs
@@ -0,0 +1,33 @@
+﻿
+using T_Store_CryptoSystem.BusinessLayer.Models;
+using T_Store_CryptoSystem.BusinessLayer.Services.Interfaces;
+
+namespace T_Store_CryptoSystem.BusinessLayer.Services;
+
+public class TransactionService : ITransactionService
+{
+    public Task<long> AddDeposit(TransactionModel transaction)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<decimal?> GetBalanceByAccountId(long accountId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TransactionModel?> GetTransactionById(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Dictionary<DateTime, List<TransactionModel>>> GetTransactionsByAccountId(long accountId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<long> Withdraw(TransactionModel transaction)
+    {
+        throw new NotImplementedException();
+    }
+}
