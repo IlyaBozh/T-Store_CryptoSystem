d5b23d7aaf096b246b2639ae74c65310dfabacd3
Ilya <ilia.il.27@mail.ru>
2023-05-22 19:54:18 +0300

    Add constructor in class TransactionService

--- T-Store_CryptoSystem.BusinessLayer/Services/TransactionService.cs
+++ T-Store_CryptoSystem.BusinessLayer/Services/TransactionService.cs
@@ -1,11 +1,27 @@
 ﻿
+using Microsoft.Extensions.Logging;
 using T_Store_CryptoSystem.BusinessLayer.Models;
 using T_Store_CryptoSystem.BusinessLayer.Services.Interfaces;
+using T_Store_CryptoSystem.DataLayer.Repository.interfaces;
 
 namespace T_Store_CryptoSystem.BusinessLayer.Services;
 
 public class TransactionService : ITransactionService
 {
+    private readonly ITransactionRepository _transactionRepository;
+    private readonly ICalculationService _calculationService;
+    private readonly ILogger<TransactionService> _logger;
+    private readonly IMapper _mapper;
+
+    public TransactionService(ITransactionRepository transactionRepository, ICalculationService calculationService,
+        IMapper mapper, ILogger<TransactionService> logger)
+    {
+        _transactionRepository = transactionRepository;
+        _calculationService = calculationService;
+        _mapper = mapper;
+        _logger = logger;
+    }
+
     public Task<long> AddDeposit(TransactionModel transaction)
     {
         throw new NotImplementedException();
