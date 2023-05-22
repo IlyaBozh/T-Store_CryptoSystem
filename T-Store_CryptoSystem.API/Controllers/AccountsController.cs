e930c08abaa79b00de5d44816c4abb3e93cb216e
Ilya <ilia.il.27@mail.ru>
2023-05-22 23:51:44 +0300

    Add AccountsController

---
+++ T-Store_CryptoSystem.API/Controllers/AccountsController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using T_Store_CryptoSystem.BusinessLayer.Services.Interfaces;
+
+namespace T_Store_CryptoSystem.API.Controllers;
+
+[ApiController]
+[Produces("application/json")]
+[Route("accounts")]
+public class AccountsController : Controller
+{
+    private readonly ITransactionService _transactionServices;
+    private readonly IMapper _mapper;
+    private readonly ILogger<AccountsController> _logger;
+
+    public AccountsController(ITransactionService transactionServices, IMapper mapper, ILogger<AccountsController> logger)
+    {
+        _transactionServices = transactionServices;
+        _mapper = mapper;
+        _logger = logger;
+    }
+}
