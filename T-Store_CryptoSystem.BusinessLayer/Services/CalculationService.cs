e68f29a082b85ced87ef3e4b91009fa4b87b14ff
Ilya <ilia.il.27@mail.ru>
2023-05-22 19:42:24 +0300

    Add method ConvertCurrency

--- T-Store_CryptoSystem.BusinessLayer/Services/CalculationService.cs
+++ T-Store_CryptoSystem.BusinessLayer/Services/CalculationService.cs
@@ -1,4 +1,5 @@
 ﻿
+using Microsoft.Extensions.Logging;
 using T_Store_CryptoSystem.BusinessLayer.Models;
 using T_Store_CryptoSystem.BusinessLayer.Services.Interfaces;
 
@@ -6,8 +7,39 @@ namespace T_Store_CryptoSystem.BusinessLayer.Services;
 
 public class CalculationService : ICalculationService
 {
-    public Task<List<TransactionModel>> ConvertCurrency(List<TransactionModel> transferModels)
+    private readonly ILogger<CalculationService> _logger;
+    private readonly IRateService _rateService;
+
+    public CalculationService(ILogger<CalculationService> logger, IRateService rateService)
     {
-        throw new NotImplementedException();
+        _logger = logger;
+        _rateService = rateService;
+    }
+
+    public async Task<List<TransactionModel>> ConvertCurrency(List<TransactionModel> transferModels)
+    {
+        var senderIndex = 0;
+        var recipientIndex = 1;
+
+        _logger.LogInformation("Business layer: Call GetCurrencyRate method");
+        var crossRate = _rateService.GetCurrencyRate(transferModels[senderIndex].Currency.ToString(), transferModels[recipientIndex].Currency.ToString());
+
+        if (transferModels[0].Currency.ToString() == RateModel.BaseCurrency || transferModels[1].Currency.ToString() == RateModel.BaseCurrency)
+        {
+            _logger.LogInformation($"Business layer: Converting {transferModels[senderIndex].Currency} to {transferModels[recipientIndex].Currency} amount {transferModels[senderIndex].Amount}");
+            transferModels[recipientIndex].Amount = transferModels[senderIndex].Currency.ToString() == RateModel.BaseCurrency ?
+            transferModels[senderIndex].Amount * crossRate : transferModels[senderIndex].Amount / crossRate;
+        }
+        else
+        {
+            _logger.LogInformation($"Business layer: Converting {transferModels[0].Currency} to {transferModels[recipientIndex].Currency} amount {transferModels[senderIndex].Amount}");
+            transferModels[recipientIndex].Amount = Math.Round((transferModels[senderIndex].Amount * crossRate),
+                                                                       4, MidpointRounding.ToNegativeInfinity);
+        }
+        transferModels[senderIndex].Amount *= -1;
+
+        _logger.LogInformation("Business layer: Transfers converted returned");
+        
+        return transferModels;
     }
 }
