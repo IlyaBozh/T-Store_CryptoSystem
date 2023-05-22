764802556d73d7e1b4d010256f841ea3c72d1405
Ilya <ilia.il.27@mail.ru>
2023-05-22 16:54:20 +0300

    Add Stored Procedure Transaction_GetAllTransactionsByAccountId

---
+++ T-Store_CryptoSystem.DB/dbo/Stored Procedures/Transaction_GetAllTransactionsByAccountId.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[Transaction_GetAllTransactionsByAccountId]
+@AccountId bigint
+AS
+BEGIN
+
+	WITH T AS
+	(
+		SELECT [Date]
+		FROM   [dbo].[Transaction]
+		WHERE  (AccountId =@AccountId )
+	)
+
+	SELECT [Id],
+		   [AccountId],
+		   [Date],
+		   [TransactionType],
+		   [Amount],
+		   [Currency]
+	FROM   [dbo].[Transaction]
+	WHERE  [Date] in (SELECT [Date] FROM T)
+END		
